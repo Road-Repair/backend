41d1232d57b7336a287d66062c626bcd00f18428
Kirill Sobolev <sblvkr@gmail.com>
2024-01-11 23:07:55 +0300

    junk removal

--- src/users/test/test_models.py
+++
@@ -1,8 +0,0 @@
-# from datetime import timedelta
-# from django.utils import timezone
-#
-# from core.fixtures import TestOrdersFixture
-#
-#
-# class TestUser(TestOrdersFixture):
-#     def test_order_creation(self):
